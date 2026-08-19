666e4a1926f6ba678e05c9b3b111f982b5b053f9
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-19 21:35:53 +0330

    Create startHandler.js

---
+++ src/handlers/startHandler.js
@@ -0,0 +1,19 @@
+import { sendMessage } from '../api/telegram.js';
+import { saveUser } from '../database/users.js';
+import { getMainMenuKeyboard } from '../keyboards/mainMenu.js';
+import { WELCOME_MESSAGE } from '../messages/welcome.js';
+
+export async function handleStart(message, env) {
+  const user = message.from;
+
+  await saveUser(env.DB, user);
+
+  const text = WELCOME_MESSAGE.replace(
+    '{firstName}',
+    user.first_name || 'دوست عزیز',
+  );
+
+  await sendMessage(message.chat.id, text, env, {
+    reply_markup: getMainMenuKeyboard(),
+  });
+    }
