3e2cea370042b7604693debbf65d6377ede68a2d
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-19 21:30:05 +0330

    Create links.js

---
+++ src/config/links.js
@@ -0,0 +1,10 @@
+export const LINKS = Object.freeze({
+  MAIN_CHANNEL: 'https://t.me/academyadminx',
+
+  EDUCATION_ADMIN: 'https://t.me/Amozesh_adminx',
+
+  OWNER_ADMIN: 'https://t.me/Owner_adminX',
+
+  SUGGESTIONS:
+    'https://t.me/XBCHATBot?start=sec-hfeiahfabd',
+});
