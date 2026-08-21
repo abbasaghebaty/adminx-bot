7ff687b94d8c21331593cca75fbb10a7743314bb
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-21 20:14:57 +0330

    Update adminsKeyboard.js

--- src/keyboards/adminsKeyboard.js
+++ src/keyboards/adminsKeyboard.js
@@ -1,17 +1,20 @@
-export function getAdminsPaginationKeyboard(page, totalPages) {
+export function getAdminsPaginationKeyboard(
+  page,
+  totalPages,
+) {
   const buttons = [];
 
   if (page > 1) {
     buttons.push({
       text: '‹ صفحه قبل',
-      callback_data: `admins:${page - 1}`,
+      callback_data: `top_admins:${page - 1}`,
     });
   }
 
   if (page < totalPages) {
     buttons.push({
       text: 'صفحه بعد ›',
-      callback_data: `admins:${page + 1}`,
+      callback_data: `top_admins:${page + 1}`,
     });
   }
 
