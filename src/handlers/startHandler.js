52c8b0ce64eedeac874c73f4baf85f02ae7f19b9
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-21 19:29:29 +0330

    Update startHandler.js

--- src/handlers/startHandler.js
+++ src/handlers/startHandler.js
@@ -1,19 +1,49 @@
 import { sendMessage } from '../api/telegram.js';
+
 import { saveUser } from '../database/users.js';
-import { getMainMenuKeyboard } from '../keyboards/mainMenu.js';
-import { WELCOME_MESSAGE } from '../messages/welcome.js';
 
-export async function handleStart(message, env) {
-  const user = message.from;
+import {
+  getMainMenuKeyboard,
+} from '../keyboards/mainMenu.js';
 
-  await saveUser(env.DB, user);
+import {
+  WELCOME_MESSAGE,
+} from '../messages/welcome.js';
 
-  const text = WELCOME_MESSAGE.replace(
-    '{firstName}',
-    user.first_name || 'دوست عزیز',
+import {
+  cancelAdminRegistration,
+} from './adminHandler.js';
+
+export async function handleStart(
+  message,
+  env,
+) {
+  await cancelAdminRegistration(
+    message,
+    env,
   );
 
-  await sendMessage(message.chat.id, text, env, {
-    reply_markup: getMainMenuKeyboard(),
-  });
-    }
+  const user = message.from;
+
+  await saveUser(
+    env.DB,
+    user,
+  );
+
+  const text =
+    WELCOME_MESSAGE.replace(
+      '{firstName}',
+      user.first_name ||
+        'دوست عزیز',
+    );
+
+  await sendMessage(
+    message.chat.id,
+    text,
+    env,
+    {
+      reply_markup:
+        getMainMenuKeyboard(),
+    },
+  );
+}
